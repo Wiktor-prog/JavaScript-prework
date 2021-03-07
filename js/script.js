c46b953b30d705d04cb40dadb1044ed569c5a3d3
Wiktor Ziejewski <wiktor.ziejewski>
2021-03-08 00:23:41 +0100

    updatejvs

--- js/script.js
+++ js/script.js
@@ -7,3 +7,12 @@ function printMessage(msg){
 function clearMessages(){
 	document.getElementById('messages').innerHTML = '';
 }
+
+var computerMove, playerMove, set_playerMove, item;
+item = String(item) + String('');
+
+set_playerMove;
+computerMove = 'kamień';
+printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
+playerMove = 'papier';
+printMessage('Zagrałem' + playerMove + '! Jeśli Twój ruch to kamień, to przegrywasz!');
