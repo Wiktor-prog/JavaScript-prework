b85ba03a99d85513f05210e54871c140ae24084e
Wiktor Ziejewski <wiktor.ziejewski>
2021-03-08 21:07:43 +0100

    updatejvs

--- js/script.js
+++ js/script.js
@@ -11,12 +11,15 @@ function clearMessages(){
 var computerMove, playerMove, set_playerMove, item;
 item = String(item) + String('');
 
+//setmove
+
 set_playerMove;
 computerMove = 'kamień';
 printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
 playerMove = 'papier';
 printMessage('Zagrałem' + playerMove + '! Jeśli Twój ruch to kamień, to przegrywasz!'); 
 
+//computermove
 
 var computerMove, randomNumber;
 randomNumber = Math.floor(Math.random() * 3 + 1);
@@ -41,3 +44,45 @@ if (randomNumber == '3') {
     computerMove = 'nieznany ruch';
 }  
 printMessage('Mój ruch: ' + computerMove);  
+
+//playermove    
+
+var playerMove, playerInput;
+playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+console.log('Wpisana odpowiedź to: ' + playerInput);
+if (playerInput == '1') {
+  playerMove = 'kamień';
+} else {
+  playerMove = 'nieznany ruch';
+}
+
+if (playerInput == '2') { 
+  playerMove = 'papier';
+} else { 
+  playerMove = 'nieznany ruch';
+}
+
+if (playerInput == '3') { 
+  playerMove = 'nożyce';
+} else { 
+  playerMove = 'nieznany ruch';
+}
+printMessage('Twój ruch: ' + playerMove); 
+
+
+var playerInput, playerMove;
+playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+console.log('Wpisana odpowiedź to: ' + playerInput);
+if (playerInput == '1') {
+  playerMove = 'kamień';
+} else if (playerInput == '2') {
+  playerMove = 'papier';
+} else {
+  if (playerInput == '3') {
+    playerMove = 'nożyce';
+  } else {
+    playerMove = 'nieznany ruch';
+  }
+}
+printMessage('Twój ruch: ' + playerMove);
+
